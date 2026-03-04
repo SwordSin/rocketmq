e1335b3cc6daf6d06845c694293112c0100438c4
zhengzhipeng <847587146@qq.com>
2026-03-04 14:41:31 +0800

    one

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
@@ -21,6 +21,9 @@ import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * This example shows how to subscribe and consume messages using providing {@link DefaultMQPushConsumer}.
@@ -28,7 +31,7 @@ import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 public class Consumer {
 
     public static final String CONSUMER_GROUP = "please_rename_unique_group_name_4";
-    public static final String DEFAULT_NAMESRVADDR = "127.0.0.1:9876";
+    public static final String DEFAULT_NAMESRVADDR = "192.168.33.122:9876";
     public static final String TOPIC = "TopicTest";
 
     public static void main(String[] args) throws MQClientException {
@@ -50,7 +53,7 @@ public class Consumer {
          * </pre>
          */
         // Uncomment the following line while debugging, namesrvAddr should be set to your local address
-        // consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
+         consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
 
         /*
          * Specify where to start in case the specific consumer group is a brand-new one.
@@ -61,12 +64,55 @@ public class Consumer {
          * Subscribe one more topic to consume.
          */
         consumer.subscribe(TOPIC, "*");
-
         /*
          *  Register callback to execute on arrival of messages fetched from brokers.
          */
+        // 顺序消费
+//        consumer.registerMessageListener((MessageListenerOrderly) (msg, context) -> {
+//            for (int i = 0; i < msg.size(); i++) {
+//                MessageExt messageExt = msg.get(i);
+//                if (messageExt.getQueueId() == 0) {
+//                    System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), new String(messageExt.getBody()));
+//                    try {
+//                        Thread.sleep(1000);
+//                        System.out.println("等待。。。。");
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                }
+//            }
+//            return ConsumeOrderlyStatus.SUCCESS;
+//        });
+
+        // 核心配置：设置每个批次的最大消费数量为 5
+        consumer.setConsumeMessageBatchMaxSize(3);
+
+        // 可选：配置拉取相关参数（辅助控制批次）
+        // 1. 每次从 Broker 拉取的最大消息数（默认 32，建议 >= consumeMessageBatchMaxSize）
+        consumer.setPullBatchSize(3);
+        // 2. 消费线程数（根据批次大小调整，比如批次5，线程数2，总并发处理10条）
+        consumer.setConsumeThreadMin(2);
+        consumer.setConsumeThreadMax(2);
+
+//        AtomicBoolean test = new AtomicBoolean(true);
         consumer.registerMessageListener((MessageListenerConcurrently) (msg, context) -> {
-            System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msg);
+            for (int i = 0; i < msg.size(); i++) {
+                MessageExt messageExt = msg.get(i);
+                String body = new String(messageExt.getBody());
+                try {
+                    System.out.println("等待一秒" + i);
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+//                if (body.contains("0") && test.get()) {
+//                    System.out.println("返回一个错误,mq重试");
+//                    System.out.printf("%s Receive New false Messages: %s %n", Thread.currentThread().getName(), body);
+//                    test.set(false);
+//                    return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+//                }
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), body);
+            }
             return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         });
 
--- example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
@@ -18,9 +18,13 @@ package org.apache.rocketmq.example.quickstart;
 
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.util.List;
 
 /**
  * This class demonstrates how to send messages to brokers using provided {@link DefaultMQProducer}.
@@ -30,9 +34,9 @@ public class Producer {
     /**
      * The number of produced messages.
      */
-    public static final int MESSAGE_COUNT = 1000;
+    public static final int MESSAGE_COUNT = 10;
     public static final String PRODUCER_GROUP = "please_rename_unique_group_name";
-    public static final String DEFAULT_NAMESRVADDR = "127.0.0.1:9876";
+    public static final String DEFAULT_NAMESRVADDR = "192.168.33.122:9876";
     public static final String TOPIC = "TopicTest";
     public static final String TAG = "TagA";
 
@@ -54,62 +58,25 @@ public class Producer {
          * </pre>
          */
         // Uncomment the following line while debugging, namesrvAddr should be set to your local address
-        // producer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
+         producer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
 
         /*
          * Launch the instance.
          */
         producer.start();
 
+        // 获取 Topic 的所有 Queue（这里只有 1 个，Queue ID 为 0）
+        List<MessageQueue> queues = producer.fetchPublishMessageQueues(TOPIC);
+        // 固定选择第 0 个 Queue（核心：所有消息都发往这个 Queue）
+        MessageQueue targetQueue = queues.get(0);
+
         for (int i = 0; i < MESSAGE_COUNT; i++) {
             try {
-
-                /*
-                 * Create a message instance, specifying topic, tag and message body.
-                 */
                 Message msg = new Message(TOPIC /* Topic */,
                     TAG /* Tag */,
                     ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
                 );
-
-                /*
-                 * Call send message to deliver message to one of brokers.
-                 */
-                SendResult sendResult = producer.send(msg, 20 * 1000);
-                /*
-                 * There are different ways to send message, if you don't care about the send result,you can use this way
-                 * {@code
-                 * producer.sendOneway(msg);
-                 * }
-                 */
-
-                /*
-                 * if you want to get the send result in a synchronize way, you can use this send method
-                 * {@code
-                 * SendResult sendResult = producer.send(msg);
-                 * System.out.printf("%s%n", sendResult);
-                 * }
-                 */
-
-                /*
-                 * if you want to get the send result in a asynchronize way, you can use this send method
-                 * {@code
-                 *
-                 *  producer.send(msg, new SendCallback() {
-                 *  @Override
-                 *  public void onSuccess(SendResult sendResult) {
-                 *      // do something
-                 *  }
-                 *
-                 *  @Override
-                 *  public void onException(Throwable e) {
-                 *      // do something
-                 *  }
-                 *});
-                 *
-                 *}
-                 */
-
+                SendResult sendResult = producer.send(msg, targetQueue);
                 System.out.printf("%s%n", sendResult);
             } catch (Exception e) {
                 e.printStackTrace();
